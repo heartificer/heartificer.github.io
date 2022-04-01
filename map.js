c6e19eec997e4db0ea27dfd228a7175510a1d5f9
Jason Cooper <563137@bah.com>
2022-04-01 14:51:45 -0400

    noticket # refactor - put location script particulars into its own file

--- map.js
+++ map.js
@@ -152,36 +152,6 @@ document.addEventListener("DOMContentLoaded", function(event) { /* begin "DOMCon
         tt.style.display = "none";
     }
 
-    function getMeatspacePosition() {
-        const statusDetails = document.querySelector("#statusDetails");
-        const locationDetails = document.querySelector("#locationDetails");
-        locationDetails.href = '';
-        locationDetails.textContent = '';
-        if (!navigator.geolocation) {
-            statusDetails.textContent = 'Geolocation is not supported by your browser';
-        } else {
-            statusDetails.textContent = 'Locating …';
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    // success method
-                    const latitude  = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    statusDetails.textContent = '';
-                    locationDetails.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-                    locationDetails.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-                },
-                () => {
-                    // error / failure method
-                    statusDetails.textContent = 'Unable to retrieve your location';
-                }
-            );
-        }
-    }
-
-    if (getMeatspacePosition && document.querySelector("#fetchLocation")){
-        document.querySelector("#fetchLocation").addEventListener('click', getMeatspacePosition);
-    }
-
     drawMap();
     drawBar("National");
     
