c7cbf722d3e3b49b7d433d7ea7d3b35d460c815d
harris-aaron-m <amharris04@email.wm.edu>
2022-04-08 14:44:16 -0400

    Update historyModel.js

--- resources/js/helpers/historyModel.js
+++ resources/js/helpers/historyModel.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) { /* begin "DOMCon
                         when: 'April Week 01',
                         actions: [
                             { who: 'Sarah', what: 'analysis documentation review and vis bug fixes'},
-                            { who: 'Aaron', what: 'visualization bug fixes, power plant distribution, zoom fixes, minor graphics tweaks, tooltip updates'},
+                            { who: 'Aaron', what: 'visualization bug fixes'},
                             { who: 'Jason', what: 'bug fixes + refactored legend'},
                         ]
                     },
