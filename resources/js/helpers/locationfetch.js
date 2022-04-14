fb16ddf8e98f0c7b3b5c618164f5bbcd63a3b5e1
heartificer <99002620+heartificer@users.noreply.github.com>
2022-04-14 13:01:47 -0400

    noticket # Wired up first pass for state detection

--- resources/js/helpers/locationfetch.js
+++ resources/js/helpers/locationfetch.js
@@ -12,11 +12,42 @@ document.addEventListener("DOMContentLoaded", function(event) { /* begin "DOMCon
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     // success method
-                    const latitude  = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    statusDetails.textContent = '';
-                    locationDetails.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-                    locationDetails.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+                    const userLatitude  = position.coords.latitude;
+                    const userLongitude = position.coords.longitude;
+                    //statusDetails.textContent = '';
+                    //locationDetails.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+                    //locationDetails.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
+                    // calculate the distance from the user to every point in each state's paths
+                    d3.json("resources/data/gz_2010_us_040_00_500k.json").then((data) =>{
+                        let stateDistances = [];
+                        data.features.forEach(state => {
+                            let name = state.properties.NAME;
+                            let coordinates = state.geometry.coordinates.join().split(',');
+                            let distances = [];
+                            for ( let i = 0; i < coordinates.length; i+=2 ) {
+                                let pathLongitude = coordinates[i];
+                                let pathLatitude = coordinates[i+1];
+                                let distance = Math.sqrt(Math.pow(pathLatitude - userLatitude, 2) + Math.pow(pathLongitude - userLongitude, 2));
+                                distances.push(distance);
+                            }
+                            // rough distance is the average of all distances
+                            let roughDistance = distances.reduce((s,c) => s += c) / distances.length;
+                            let stateDistance = { name, roughDistance}
+                            stateDistances.push(stateDistance)
+                        });
+                        if (stateDistances.length > 0) {
+                            stateDistances.sort((a,b) => {
+                                if ( a.roughDistance >= b.roughDistance ) {
+                                    return 1;
+                                } else {
+                                    return -1;
+                                }
+                            });
+                            let userState = stateDistances[0].name;
+                            statusDetails.textContent = `Are you roughly here: ${userState} ?`;
+                        }
+                    })
                 },
                 () => {
                     // error / failure method
@@ -26,9 +57,12 @@ document.addEventListener("DOMContentLoaded", function(event) { /* begin "DOMCon
         }
     }
 
+    /*
     if (getMeatspacePosition && document.querySelector("#fetchLocation")){
         document.querySelector("#fetchLocation").addEventListener('click', getMeatspacePosition);
     }
+    //*/
+    getMeatspacePosition();
 
 
   } /* cease "DOMContentLoaded" event */
